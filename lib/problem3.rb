39189852ba0f6608ff46d7e87abbb97ab1ed1ec5
tech-a44 <robin.mck11@hotmail.co.uk>
2018-10-15 20:38:02 +0100

    Completed

--- lib/problem3.rb
+++ lib/problem3.rb
@@ -1,8 +1,24 @@
 class Problem3
-  attr_accessor :number_to_factorize, :factors
+  attr_accessor :num_to_factorize, :factor
 
-  def initialize number_to_factorize
-    @number_to_factorize = number_to_factorize
-    @factors = []
+  def initialize num
+    @num_to_factorize = num
   end
+
+  def largest_factor
+
+    #Initialize as two
+
+    @factor = 2
+
+    while @num_to_factorize > @factor
+      if @num_to_factorize % @factor == 0
+        @num_to_factorize /= @factor
+        @factor = 2
+      else
+        @factor+=1
+      end
+    end
+  end
+
 end
--- spec/problem3_spec.rb
+++ spec/problem3_spec.rb
@@ -3,19 +3,16 @@ require_relative '../lib/problem3.rb'
 describe "problem3" do
   before(:all) do
     @problem3 = Problem3.new 13195
+    @problem3.largest_factor
   end
 
   it "should be an integer greater than zero" do
-    expect(@problem3.number_to_factorize).to be_kind_of(Integer)
-    expect(@problem3.number_to_factorize).to be > 0
+    expect(@problem3.num_to_factorize).to be_kind_of(Integer)
+    expect(@problem3.num_to_factorize).to be > 0
   end
 
-  it "should be an array" do
-    expect(@problem3.factors).to be_kind_of(Array)
-  end
-
-  it "should be the largest element in the array" do
-    expect(@problem3.factors.max).to eq(@problem3.factors.last)
+  it "should be 29" do
+    expect(@problem3.factor).to eq 29
   end
 
 
